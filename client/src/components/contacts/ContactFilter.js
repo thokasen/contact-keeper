6c631e69decfece730877e74f030f581b2025855
unknown <vaag1991@gmail.com>
2020-01-29 21:42:33 +0100

    client side setup & contacts ui

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,43 @@
+import React, { useContext, useRef, useEffect } from "react";
+import ContactContext from "../../context/contact/contactContext";
+const ContactFilter = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { filterContacts, clearFilter, filtered } = contactContext;
+  const text = useRef(null);
+
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = "";
+    } else {
+    }
+  });
+
+  const onChange = e => {
+    // if (e.target.value !== "") {
+    //   filterContacts(e.target.value);
+    // } else {
+    //   clearFilter();
+    // }
+
+    console.log(text);
+    if (text.current.value !== "") {
+      filterContacts(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
+
+  return (
+    <form>
+      <input
+        ref={text}
+        type='text'
+        onChange={onChange}
+        placeholder='Filter Contacts...'
+      />
+    </form>
+  );
+};
+
+export default ContactFilter;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,19 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+import ContactFilter from "../contacts/ContactFilter";
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className='grid-2'>
+      <div>
+        <ContactForm />
+      </div>
+      <div>
+        <ContactFilter />
+        <Contacts />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
